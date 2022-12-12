a031bdbf7a4215d4fce8b2d4f1bc118dd404006e
Wojciech Zyndul <242575@edu.p.lodz.pl>
2022-12-12 13:27:35 +0100

    zadanie 8

--- SudokuGameProject/View/src/main/java/pl/comp/viewproject/SceneController.java
+++ SudokuGameProject/View/src/main/java/pl/comp/viewproject/SceneController.java
@@ -42,4 +42,11 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+    public void switchToScenegame(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("game.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
